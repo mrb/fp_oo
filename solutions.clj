d991f0ef20e55fc07a346fd831d51e443eeb04b6
mrb <michaelrbernstein@gmail.com>
2013-01-25 22:21:47 -0500

    Most of Chapter 5

--- solutions.clj
+++ solutions.clj
@@ -145,3 +145,61 @@
        :y :y}}))
 
 ;;; Chapter 5 - Moving the Class Out of the Constructor
+
+;; Exercise 1
+(def apply-message-to
+  (fn [class instance message args]
+    (apply (message (:__instance_methods__ class)) instance args)))
+
+(def make
+  (fn [class & args]
+    (let [seeded {:__class_symbol__ (:__own_symbol__ class)}]
+      (apply-message-to class seeded :add-instance-values args))))
+
+(def send-to
+  (fn [instance message & args]
+    (let [class (eval (:__class_symbol__ instance))]
+      (apply-message-to class instance message args))))
+
+;; Exercise 2
+(def Point
+{
+  :__own_symbol__ 'Point
+  :__instance_methods__
+  {
+    :add-instance-values (fn [this x y]
+                           (assoc this :x x :y y))
+    :class-name  :__class_symbol__
+    :class (fn [this] (eval (:__class_symbol__ this)))
+    :shift (fn [this xinc yinc]
+             (make Point (+ (:x this) xinc)
+                         (+ (:y this) yinc)))
+    :add (fn [this other]
+           (send-to this :shift (:x other)
+                                (:y other)))
+   }
+ })
+
+;; Exercise 3
+(def Point
+{
+  :__own_symbol__ 'Point
+  :__instance_methods__
+  {
+    :add-instance-values (fn [this x y]
+                           (assoc this :x x :y y))
+    :class-name  :__class_symbol__
+    :class (fn [this] (eval (:__class_symbol__ this)))
+    :shift (fn [this xinc yinc]
+             (make Point (+ (:x this) xinc)
+                         (+ (:y this) yinc)))
+    :add (fn [this other]
+           (send-to this :shift (:x other)
+                                (:y other)))
+    :origin (fn [this] (make Point 0 0))
+   }
+ })
+
+;; Instances created before class redefinition benefit from the dynamic nature
+;; of the method dispatch in the system we're working on. Because the instances
+;; have a reference to the class and don't hold methods themselves, this works.
